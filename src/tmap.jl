8dabdf5f37c5e07769a8f6a992b88aa45784b5a9
Marek Dědič <developer@dedic.eu>
2017-06-08 17:02:22 +0200

    Added partial support for type-changing functions

--- src/tmap.jl
+++ src/tmap.jl
@@ -1,18 +1,21 @@
 export tmap, tmap!;
 
-function tmap{T<:AbstractArray}(f::Function, c::T)::T
-	d = deepcopy(c);
-	tmap!(f, d);
-	return d;
+function tmap{T<:AbstractArray}(f::Function, c::T)
+	ret = similar(c, Any);
+	Threads.@threads for i in eachindex(c)
+		ret[i] = f(c[i]);
+	end
+	return reshape([ret...],size(ret)...);
 end
 
-function tmap!{T<:AbstractArray}(f::Function, c::T)::Void
+function tmap!{T<:AbstractArray}(f::Function, c::T)::Void # TODO: Handle type instability
 	Threads.@threads for i in eachindex(c)
 		c[i] = f(c[i]);
 	end
 end
 
-function tmap!{T<:AbstractArray}(f::Function, d::T, c::T)::Void
-	d = deepcopy(c);
-	tmap!(f, d);
+function tmap!{T<:AbstractArray}(f::Function, d::T, c::T)::Void # TODO: Handle type instability
+	Threads.@threads for i in eachindex(c)
+		d[i] = f(c[i]);
+	end
 end
