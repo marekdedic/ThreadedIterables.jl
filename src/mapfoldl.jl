b6ba15ce570b48ff3ace646633599b08c42e4f6f
Marek Dědič <developer@dedic.eu>
2018-09-25 14:59:52 +0200

    moved v0 to init in mapfoldl and mapfoldr

--- src/mapfoldl.jl
+++ src/mapfoldl.jl
@@ -1,12 +1,12 @@
 export tmapfoldl;
 
 """
-    tmapfoldl(f::Function, op::Function, v0, itr::AbstractArray)::AbstractArray
+    tmapfoldl(f::Function, op::Function, itr::AbstractArray; init)::AbstractArray
 
-Multi-threaded version of [mapfoldl(f, op, v0, itr)](https://docs.julialang.org/en/stable/stdlib/collections/#Base.mapfoldl). Only the **mapping** part is parallelised.
+Multi-threaded version of [mapfoldl(f, op, itr; init)](https://docs.julialang.org/en/stable/stdlib/collections/#Base.mapfoldl). Only the **mapping** part is parallelised.
 """
-function tmapfoldl(f::Function, op::Function, v0, itr::T) where T<:AbstractArray
-	return foldl(op, v0, tmap(f, itr));
+function tmapfoldl(f::Function, op::Function, itr::T; init) where T<:AbstractArray
+	return foldl(op, tmap(f, itr); init = init);
 end
 
 """
--- src/mapfoldr.jl
+++ src/mapfoldr.jl
@@ -1,12 +1,12 @@
 export tmapfoldr;
 
 """
-    tmapfoldr(f::Function, op::Function, v0, itr::AbstractArray)::AbstractArray
+    tmapfoldr(f::Function, op::Function, itr::AbstractArray; init)::AbstractArray
 
-Multi-threaded version of [mapfoldr(f, op, v0, itr)](https://docs.julialang.org/en/stable/stdlib/collections/#Base.mapfoldr). Only the **mapping** part is parallelised.
+Multi-threaded version of [mapfoldr(f, op, itr; init)](https://docs.julialang.org/en/stable/stdlib/collections/#Base.mapfoldr). Only the **mapping** part is parallelised.
 """
-function tmapfoldr(f::Function, op::Function, v0, itr::T) where T<:AbstractArray
-	return foldr(op, v0, tmap(f, itr));
+function tmapfoldr(f::Function, op::Function, itr::T; init) where T<:AbstractArray
+	return foldr(op, tmap(f, itr); init = init);
 end
 
 """
