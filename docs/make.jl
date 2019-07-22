fd763b12f334e6726a53c0ea22d47a1b6bd90b18
Marek Dědič <madedic@cisco.com>
2019-07-22 11:10:50 +0200

    deploying the docs manually

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,14 @@ makedocs(
 		]
 );
 
-deploydocs(
-	repo = "github.com/marekdedic/ThreadedMap.jl.git"
-);
+sha = try
+	readchomp(`git rev-parse --short HEAD`)
+catch
+	"(not-git-repo)"
+end
+
+mktempdir() do temp
+	# TODO: Actually run the job on tags
+	# TODO: key
+	git_push(Utilities.currentdir(), temp, "github.com/marekdedic/ThreadedMap.jl.git"; target = "build", tag = get(ENV, "CIRCLE_TAG", ""), sha = sha, versions = ["stable" => "v^", "v#.#", "dev" => "dev"]);
+end
