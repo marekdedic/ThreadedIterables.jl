fbdbec9be56fb128e0c3f72d39b7fe693416091e
Marek Dědič <madedic@cisco.com>
2019-07-24 10:58:45 +0200

    CI: Split docs building from docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -16,15 +16,3 @@ makedocs(
 			"mapfoldr.md"]
 		]
 );
-
-if get(ENV, "CIRCLECI", "") == "true"
-	sha = try
-		readchomp(`git rev-parse --short HEAD`)
-	catch
-		"(not-git-repo)"
-	end
-
-	mktempdir() do temp
-		Documenter.git_push(Documenter.Utilities.currentdir(), temp, "github.com/marekdedic/ThreadedMap.jl.git"; target = "docs/build", tag = get(ENV, "CIRCLE_TAG", ""), sha = sha, versions = ["stable" => "v^", "v#.#", "dev" => "dev"]);
-	end
-end
