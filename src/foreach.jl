46fd1f60ee4bde3e422aded80eefdc89a25fa888
Marek Dědič <madedic@cisco.com>
2019-09-25 17:24:04 +0200

    Updated Julia documentation links

--- src/foreach.jl
+++ src/foreach.jl
@@ -3,7 +3,7 @@ export tforeach;
 """
 	tforeach(f::Function, c::AbstractArray)::Nothing
 
-Multi-threaded version of [foreach(f, c)](https://docs.julialang.org/en/v1.1/base/collections/#Base.foreach).
+Multi-threaded version of [foreach(f, c)](https://docs.julialang.org/en/v1.2/base/collections/#Base.foreach).
 """
 function tforeach(f::Function, c...)::Nothing
 	ensureThreaded();
--- src/mapfoldr.jl
+++ src/mapfoldr.jl
@@ -3,7 +3,7 @@ export tmapfoldr;
 """
     tmapfoldr(f::Function, op::Function, itr::AbstractArray; init)::AbstractArray
 
-Multi-threaded version of [mapfoldr(f, op, itr; init)](https://docs.julialang.org/en/v1.1/base/collections/#Base.mapfoldr-Tuple{Any,Any,Any}). Only the **mapping** part is parallelised.
+Multi-threaded version of [mapfoldr(f, op, itr; init)](https://docs.julialang.org/en/v1.2/base/collections/#Base.mapfoldr-Tuple{Any,Any,Any}). Only the **mapping** part is parallelised.
 """
 function tmapfoldr(f::Function, op::Function, itr::T; init) where T<:AbstractArray
 	return foldr(op, tmap(f, itr); init = init);
@@ -12,7 +12,7 @@ end
 """
     tmapfoldr(f::Function, op::Function, itr::AbstractArray)::AbstractArray
 
-Multi-threaded version of [mapfoldr(f, op, itr)](https://docs.julialang.org/en/v1.1/base/collections/#Base.mapfoldr-Tuple{Any,Any,Any}). Only the **mapping** part is parallelised.
+Multi-threaded version of [mapfoldr(f, op, itr)](https://docs.julialang.org/en/v1.2/base/collections/#Base.mapfoldr-Tuple{Any,Any,Any}). Only the **mapping** part is parallelised.
 """
 function tmapfoldr(f::Function, op::Function, itr::T) where T<:AbstractArray
 	return foldr(op, tmap(f, itr));
