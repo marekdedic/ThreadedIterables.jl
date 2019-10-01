70718a513f078bd3962638f4829cc794751d5af2
Marek Dědič <madedic@cisco.com>
2019-10-01 16:20:53 +0200

    Fixed @threaded macro scopes

--- src/threadedMacro.jl
+++ src/threadedMacro.jl
@@ -1,9 +1,17 @@
 export @threaded;
 
 macro threaded(expr::Expr)
-	local dict = Dict{Symbol, Symbol}(:foreach => :tforeach, :mapfoldl => :tmapfoldl, :mapfoldr => :tmapfoldr, :map => :tmap, :map! => :tmap!, :mapreduce => :tmaptreduce, :reduce => :treduce);
+	local dict = Dict{Symbol, Symbol}(
+		:foreach => :tforeach,
+		:mapfoldl => :tmapfoldl,
+		:mapfoldr => :tmapfoldr,
+		:map => :tmap,
+		:map! => :tmap!,
+		:mapreduce => :tmaptreduce,
+		:reduce => :treduce
+	);
 
-	local function replace!(expr::Expr)
+	local function replace_threaded(expr::Expr)
 		if expr.head == :call
 			try
 				expr.args[1] = dict[expr.args[1]];
@@ -12,11 +20,13 @@ macro threaded(expr::Expr)
 		end
 		for arg in expr.args
 			if typeof(arg) == Expr
-				replace!(arg);
+				replace_threaded(arg);
 			end
 		end
+		return expr;
 	end
 
-	replace!(expr);
-	return expr;
+	return quote
+		$(esc(replace_threaded(expr)))
+	end
 end
